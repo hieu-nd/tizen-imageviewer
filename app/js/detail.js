7ea7a6b6cafa718ae4b3df8768c12e3fbbd0b5d3
hieu duc nguyen <hieu.nguyenduc.hcm@gmail.com>
2016-03-01 14:25:12 +0700

    update flip, rotate and autoplay with background and music

--- app/js/detail.js
+++ app/js/detail.js
@@ -1,17 +1,62 @@
 $(document).ready(function() {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', 'media/Forever.mp3');
+    var $fotoramaDiv = $('#fotorama').fotorama();
+    var fotorama = $fotoramaDiv.data('fotorama');
+    $("#light-overlay").hide();
 
-   
+    $("#enable").on("click", function() {
+        $(this).hide();
+        $("#light-overlay").show();
+        $("#disable").show();
+        audioElement.play();
 
-   
+        fotorama.startAutoplay(2000);
+    })
+    $("#disable").on("click", function() {
+        $(this).hide();
+        $("#light-overlay").hide();
+        $("#enable").show();
+        audioElement.pause();
+        fotorama.stopAutoplay();
+    })
 
+    $("#div-flip").on("click", function() {
+        $(imageactive).removeClass("imageflipped");
+        var imageactive = $(".fotorama__stage__shaft .fotorama__active img");
+        if ($(imageactive).hasClass("imageflipped")) {
+            $(imageactive).removeClass("imageflipped");
+        } else {
+            $(imageactive).removeAttr('class');
+            $(imageactive).addClass("imageflipped");
+        }
+    })
+    var i = 0;
+    $("#div-rotate").on("click", function() {
+        i += 1;
+        if (i > 4) {
+            i = 0;
+        }
+        var imageactive = $(".fotorama__stage__shaft .fotorama__active img");
+        $(imageactive).removeAttr('class');
+        switch (i) {
+            case 1:
+                $(imageactive).addClass("rotate90");
+                break;
+            case 2:
+                $(imageactive).addClass("rotate180");
+                break;
+            case 3:
+                $(imageactive).addClass("rotate270");
+                break;
+            case 4:
+                $(imageactive).addClass("rotate360");
+                break;
+            default:
+                $(imageactive).addClass("rotate90");
+                break;
+        }
+
+
+    });
 });
-$("#enable").click(function(){
-    $(this).hide();
-    $("#right-coontainer").addClass("opacity");
-    $("#disable").show();
-})
-$("#disable").click(function(){
-    $(this).hide();
-    $("#right-coontainer").removeClass("opacity");
-    $("#enable").show();
-})
